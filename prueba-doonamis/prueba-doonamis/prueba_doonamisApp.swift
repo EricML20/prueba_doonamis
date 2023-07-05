b3da0b9e6a5dbab3e895f21d107e31c28baf3bda
EricML20 <ericmele20@gmail.com>
2023-07-05 12:44:40 +0200

    Project started

---
+++ prueba-doonamis/prueba-doonamis/prueba_doonamisApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba_doonamisApp.swift
+//  prueba-doonamis
+//
+//  Created by Eric Melé Lorite on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct prueba_doonamisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
