8a9f26b4f4607efc9c769c0a541f3a1cd60ac4e7
EricML20 <ericmele20@gmail.com>
2023-07-06 14:03:35 +0200

    Version 1.0

--- prueba-doonamis/prueba-doonamis/ContentView.swift
+++ prueba-doonamis/prueba-doonamis/ContentView.swift
@@ -7,15 +7,93 @@
 
 import SwiftUI
 
+struct Movie: Identifiable {
+    let id: Int
+    let name: String
+    let popularity: CGFloat
+    let overview: String
+    let backdrop_path: String
+    let first_air_date: String
+    let vote_average: CGFloat
+    let vote_count: Int
+    let genre_ids: [Int]
+    let poster_path: String
+}
+
 struct ContentView: View {
+    
+    @State private var movies: [Movie] = []
+    
+    func handleTVShows() {
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.themoviedb.org/3/tv/popular?language=en-US&page=1&api_key=c6aeee577586ba38e487b74dfede5deb")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+          if (error != nil) {
+            print(error as Any)
+          } else {
+            let httpResponse = response as? HTTPURLResponse
+              //print(httpResponse)
+              if let data = data {
+                  do {
+                      if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                          print("JSON Response:\n\(json)")
+                          if let jsonMovies = json["results"] as? [[String: Any]] {
+                                      movies = jsonMovies.compactMap { movieData in
+                                          guard let id = movieData["id"] as? Int,
+                                                let name = movieData["name"] as? String,
+                                                let popularity = movieData["popularity"] as? CGFloat,
+                                                let overview = movieData["overview"] as? String,
+                                                let backdrop_path = movieData["backdrop_path"] as? String,
+                                                let first_air_date = movieData["first_air_date"] as? String,
+                                                let vote_average = movieData ["vote_average"] as? CGFloat,
+                                                let vote_count = movieData["vote_count"] as? Int,
+                                                let genre_ids = movieData["genre_ids"] as? [Int],
+                                                let poster_path = movieData["poster_path"] as? String
+                                          else {
+                                              return nil
+                                          }
+                                          return Movie(id: id, name: name, popularity: popularity, overview: overview, backdrop_path: backdrop_path, first_air_date: first_air_date, vote_average: vote_average, vote_count: vote_count, genre_ids: genre_ids, poster_path: poster_path)
+                                      }
+                                      movies.sort(by: { $0.popularity > $1.popularity })
+                                  }
+                      }
+                  } catch {
+                      print("Error converting data to JSON: \(error)")
+                  }
+              }
+          }
+        })
+
+        dataTask.resume()
+        
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView() {
+            List(movies) { movie in
+                HStack {
+                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w92/" + movie.backdrop_path))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 60, height: 60)
+                        .scaledToFit()
+                        .clipped()
+                    
+                    NavigationLink(destination: DetailsMovies(selected_movie: movie)){
+                        Text(movie.name)
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Most popular movies")
+            
         }
-        .padding()
+        .onAppear {
+            handleTVShows()
+        }
     }
 }
 
---
+++ prueba-doonamis/prueba-doonamis/DetailsMovies.swift
@@ -0,0 +1,117 @@
+//
+//  DetailsMovies.swift
+//  prueba-doonamis
+//
+//  Created by Eric Melé Lorite on 5/7/23.
+//
+
+import SwiftUI
+
+struct Genres: Identifiable {
+    let id: Int
+    let name: String
+}
+
+struct DetailsMovies: View {
+    
+    @State private var genres: [Genres] = []
+    @State var selected_movie: Movie
+    
+    func getGenres() {
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.themoviedb.org/3/genre/tv/list?language=en&api_key=c6aeee577586ba38e487b74dfede5deb")! as URL,
+                                          cachePolicy: .useProtocolCachePolicy,
+                                          timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error as Any)
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                //print(httpResponse)
+                if let data = data {
+                    do {
+                        if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                            if let jsonMovies = json["genres"] as? [[String: Any]] {
+                                genres = jsonMovies.compactMap { movieData in
+                                    if let id = movieData["id"] as? Int,
+                                       let name = movieData["name"] as? String {
+                                        return Genres(id: id, name: name)
+                                    }
+                                    return nil
+                                }
+                            }
+                        }
+                    } catch {
+                        print("Error al convertir los datos en JSON: \(error)")
+                    }
+                }
+                
+            }
+        })
+        
+        dataTask.resume()
+    }
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text(selected_movie.name)
+                    .font(.title2)
+                
+                HStack() {
+                    Spacer()
+                    
+                    Label("\(selected_movie.popularity, specifier: "%.2f")", systemImage: "chart.line.uptrend.xyaxis")
+                    
+                    Label("\(selected_movie.vote_count)", systemImage: "person.3")
+                    
+                    Label("\(selected_movie.vote_average, specifier: "%.1f")", systemImage: "heart")
+                    
+                    Spacer()
+                }
+                .font(.caption)
+            }
+            HStack {
+                VStack(alignment: .leading) {
+                    AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w342/" + selected_movie.poster_path))
+                    
+                    Text(selected_movie.genre_ids.compactMap { genreID in
+                        genres.first { $0.id == genreID }?.name
+                        
+                    }.joined(separator: ", "))
+                    .font(.footnote)
+                }
+            }
+            
+            VStack(alignment: .leading) {
+                if !selected_movie.overview.isEmpty {
+                    Text("Overview")
+                        .font(.headline)
+               
+                    Text(selected_movie.overview)
+                        .font(.body)
+                }
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(maxWidth: .infinity, alignment: .leading) 
+            .padding(.horizontal, 25)
+            .padding(.top, 10)
+            
+            Spacer()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            getGenres()
+            
+        }
+        
+    }
+    
+    struct DetailsMovies_Previews: PreviewProvider {
+        static var previews: some View {
+            DetailsMovies(selected_movie: Movie(id: 134224, name: "Voltes V: Legacy", popularity: 1865.967, overview: "Voltes V: Legacy follows the story of three brothers, Steve, Big Bert, and Little Jon Armstrong, and their friends Jamie Robinson and Mark Gordon, as they fight the forces of humanoid aliens known as Boazanians who plans to invade the earth and launch their beast fighters all over the world.", backdrop_path: "/fEP2Sha3PXJnAcoGOjr5oybq44Q.jpg", first_air_date: "2023-05-08", vote_average: 5.3, vote_count: 6, genre_ids: [10765,10759], poster_path: "/jZ32tdCTFsRoU2GehW6ZzSpTyS1.jpg"))
+        }
+    }
+}
